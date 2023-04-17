be75f4b89e13ef318291ee52b19eadf8d7c8fa12
Auro de Oliveira Junior <auroojr@gmail.com>
2023-04-17 15:17:40 -0300

    Corrigido o tratamento nas variaveis de busca por endereço

--- menu.py
+++ menu.py
@@ -29,14 +29,17 @@ def apontar_funcoes(valor):
 
     elif valor == '2':
         uf = input('Insira o Estado: ')  # minimo dois caracteres
-        while uf.isalpha() == False or len(uf) < 2 or len(uf) > 2:
+        while uf.isalpha() == False or len(uf) < 2 or len(uf) > 2:  # != 2
             print('Insira a silgla do Estado - minimo 2 caracteres')
             uf = input('Insira o Estado: ')
 
         cidade = input('Insira a Cidade: ')  # minimo dois caracteres
-        while cidade.isalpha() == False and len(cidade) < 1:
+        while len(cidade) < 2 or not cidade.isalpha():
             print('Insira o nome da cidade - Minimo 2 caracteres')
             cidade = input('Insira a Cidade: ')
+
+        print(len(cidade))
+        print(cidade.isalpha())
 
         endereco = input('Insira o Endereço ')  # minimo tres caracateres
         while len(endereco) <= 2:
