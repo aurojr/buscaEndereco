3fb146d4d80f2eb1d094f9a6f8f1b611f23bc9db
Marco Túlio Zuquim <mtulio.z@gmail.com>
2023-04-18 15:16:51 -0300

    fix: removing unnecessary function call

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ def main():
     while opcao != '3':
         menu()
         opcao = input('Escolha usa opção: ')
-        valor = escolher_opcao(opcao)
-        apontar_funcoes(valor)
+        apontar_funcoes(opcao)
         print()
 
 
