5181c9e957e8f9a22cdb71f9f1a1af899fa0718b
Marco Túlio Zuquim <mtulio.z@gmail.com>
2023-04-18 14:52:09 -0300

    fix: implementing __name__ == "__main__"

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from menu import menu, escolher_opcao, apontar_funcoes
 
-opcao = ''
-while opcao != '3':
-    menu()
-    opcao = input('Escolha usa opção: ')
-    valor = escolher_opcao(opcao)
-    apontar_funcoes(valor)
-    print()
+
+def main():
+    opcao = ''
+    while opcao != '3':
+        menu()
+        opcao = input('Escolha usa opção: ')
+        valor = escolher_opcao(opcao)
+        apontar_funcoes(valor)
+        print()
+
+
+if __name__ == '__main__':
+    main()
